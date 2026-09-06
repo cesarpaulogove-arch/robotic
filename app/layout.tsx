0bfdb6ec8367702e7046a13631e0ca832a1a0b5a
cesarpaulogove-arch <cesarpaulogove@gmail.com>
2026-09-06 18:34:55 +0200

    actualizado 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
+import type { Metadata } from "next";
 import "./globals.css";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 
+export const metadata: Metadata = {
+  title: "WScode e Protótipos",
+  description:
+    "Iniciativa freelancer dedicada ao desenvolvimento de código e protótipos, apoiando estudantes na criação e aperfeiçoamento dos seus projetos e no desenvolvimento de competências tecnológicas.",
+};
 
-
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
-
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export default function RootLayout({
   children,
@@ -14,12 +21,16 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
-      className={cn("h-full", "antialiased", "font-sans", geist.variable)}
+      lang="pt"
+      className={cn(
+        "h-full",
+        "antialiased",
+        "font-sans",
+        geist.variable
+      )}
     >
       <body className="min-h-full flex flex-col">
         {children}
-      
       </body>
     </html>
   );
